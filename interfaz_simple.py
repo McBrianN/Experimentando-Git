4ba242e94eeeefafbd142bf745d4d92d4cfa10b3
McBriann <brianluismussin@gmail.com>
2025-04-04 22:45:31 -0300

    cambio numero 3

--- interfaz_simple.py
+++ interfaz_simple.py
@@ -23,7 +23,7 @@ class Ventana(QWidget):
         self.show()
     
     def contenido(self):
-        titulo = QLabel("Los burros del otro canal son unos panchitos")
+        titulo = QLabel("Aprendiendo GIT")
         titulo.setAlignment(Qt.AlignmentFlag.AlignCenter)
         titulo.setFont(QFont("Times New Roman", 25))
         titulo.setStyleSheet("color: orange;")
