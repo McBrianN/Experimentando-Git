691458404154bfb4792426c8062f8b38903e94d1
Lucila <400712@sistemas.frc.utn.edu.ar>
2025-04-04 23:15:15 -0300

    Cambios en el titulo

--- interfaz_simple.py
+++ interfaz_simple.py
@@ -23,7 +23,7 @@ class Ventana(QWidget):
         self.show()
     
     def contenido(self):
-        titulo = QLabel("Aprendiendo a resolver conflictos de GIT!")
+        titulo = QLabel("Cambios de Lu")
         titulo.setAlignment(Qt.AlignmentFlag.AlignCenter)
         titulo.setFont(QFont("Times New Roman", 25))
         titulo.setStyleSheet("color: orange;")
